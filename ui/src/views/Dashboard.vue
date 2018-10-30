08ea344b88fd1490dd8dc9ff545af82ad8c5555f
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2018-10-30 12:50:51 +0530

    fixing the path issue

--- ui/src/views/Dashboard.vue
+++ ui/src/views/Dashboard.vue
@@ -32,7 +32,7 @@
 
 <script>
 import Account from "../components/Account";
-import Explorer from "../components/explorer/explorer.vue";
+import Explorer from "../components/Explorer/Explorer.vue";
 import Popup from "../components/Popup/Popup";
 import Button from "../components/Form/Button";
 import DeleteFolder from "../components/Folder/DeleteFolder";
